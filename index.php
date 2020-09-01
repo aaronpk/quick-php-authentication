ff0be3461c3f6a82fb6cc0e2f279c29f38bffc5d
Aaron Parecki <aaron@parecki.com>
2020-09-01 10:08:22 -0700

    add pkce, reorder code, use openid userinfo endpoint

--- index.php
+++ index.php
@@ -1,34 +1,50 @@
 <?php
 session_start();
 
-function http($url, $params=false) {
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  if($params)
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-  return json_decode(curl_exec($ch));
-}
+$client_id = '';
+$client_secret = '';
+$redirect_uri = 'http://localhost:8080/';
+$metadata_url = 'https://*.okta.com/oauth2/default/.well-known/openid-configuration';
+
+
 
 if(isset($_GET['logout'])) {
   unset($_SESSION['username']);
+  unset($_SESSION['sub']);
   header('Location: /');
   die();
 }
 
-if(isset($_SESSION['username'])) {
+if(isset($_SESSION['sub'])) {
   echo '<p>Logged in as</p>';
   echo '<p>' . $_SESSION['username'] . '</p>';
   echo '<p><a href="/?logout">Log Out</a></p>';
   die();
 }
 
-$client_id = '';
-$client_secret = '';
-$redirect_uri = 'http://localhost:8080/';
-$metadata_url = 'https://dev-123456.oktapreview.com/oauth2/default/.well-known/oauth-authorization-server';
+
 $metadata = http($metadata_url);
 
-if(isset($_GET['code'])) {
+if(!isset($_GET['code'])) {
+
+  $_SESSION['state'] = bin2hex(random_bytes(5));
+  $_SESSION['code_verifier'] = bin2hex(random_bytes(50));
+  $code_challenge = base64_urlencode(hash('sha256', $_SESSION['code_verifier'], true));
+
+  $authorize_url = $metadata->authorization_endpoint.'?'.http_build_query([
+    'response_type' => 'code',
+    'client_id' => $client_id,
+    'redirect_uri' => $redirect_uri,
+    'state' => $_SESSION['state'],
+    'scope' => 'openid profile',
+    'code_challenge' => $code_challenge,
+    'code_challenge_method' => 'S256',
+  ]);
+
+  echo '<p>Not logged in</p>';
+  echo '<p><a href="'.$authorize_url.'">Log In</a></p>';
+
+} else {
 
   if($_SESSION['state'] != $_GET['state']) {
     die('Authorization server returned an invalid state parameter');
@@ -44,38 +60,41 @@ if(isset($_GET['code'])) {
     'redirect_uri' => $redirect_uri,
     'client_id' => $client_id,
     'client_secret' => $client_secret,
+    'code_verifier' => $_SESSION['code_verifier'],
   ]);
 
   if(!isset($response->access_token)) {
     die('Error fetching access token');
   }
 
-  $token = http($metadata->introspection_endpoint, [
-    'token' => $response->access_token,
-    'client_id' => $client_id,
-    'client_secret' => $client_secret,
+  $userinfo = http($metadata->userinfo_endpoint, [
+    'access_token' => $response->access_token,
   ]);
 
-  if($token->active == 1) {
-    $_SESSION['username'] = $token->username;
+  if($userinfo->sub) {
+    $_SESSION['sub'] = $userinfo->sub;
+    $_SESSION['username'] = $userinfo->preferred_username;
+    $_SESSION['profile'] = $userinfo;
     header('Location: /');
     die();
   }
+
 }
 
-if(!isset($_SESSION['username'])) {
-  $_SESSION['state'] = bin2hex(random_bytes(5));
 
-  $authorize_url = $metadata->authorization_endpoint.'?'.http_build_query([
-    'response_type' => 'code',
-    'client_id' => $client_id,
-    'redirect_uri' => $redirect_uri,
-    'state' => $_SESSION['state'],
-    'scope' => 'openid',
-  ]);
-  #$authorize_url = 'TODO';
 
-  echo '<p>Not logged in</p>';
-  echo '<p><a href="'.$authorize_url.'">Log In</a></p>';
+// Base64-urlencoding is a simple variation on base64-encoding
+// Instead of +/ we use -_, and the trailing = are removed.
+function base64_urlencode($string) {
+  return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
+}
+
+
+function http($url, $params=false) {
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  if($params)
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+  return json_decode(curl_exec($ch));
 }
 
